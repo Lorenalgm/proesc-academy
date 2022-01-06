2e761427b339968103e3921ad37bf7de0a43f50d
Luma Montes <lumagoesmontes@gmail.com>
2022-01-06 19:22:08 -0300

    ajustado model

--- backend/app/Models/CursoFaseAulaMaterial.php
+++ backend/app/Models/CursoFaseAulaMaterial.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class CursosFasesAulasMateriais extends Model
 {
     use HasFactory;
+
+    protected $table = 'cursos_fases_aulas_materiais';
+
+    protected $fillable = [
+        'curso_fase_aula_id',
+        'link',
+        'nome'
+    ];
 }
