b7fa22eb4ec2feed90c15c5e707449fe6f20bc04
Douglas Spíndola <douglas.spindola15@gmail.com>
2022-01-06 20:13:24 -0300

    Corrigido nome da class

--- backend/app/Http/Controllers/SetoresController.php
+++ backend/app/Http/Controllers/SetoresController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Setor;
 
-class SetorController extends Controller
+class SetoresController extends Controller
 {
     /**
      * Display a listing of the resource.
