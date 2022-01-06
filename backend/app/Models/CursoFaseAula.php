eb052f104356a11d74000a3f28837d016875da24
Luma Montes <lumagoesmontes@gmail.com>
2022-01-06 19:19:46 -0300

    criado models e controllers

---
+++ backend/app/Models/CursoFaseAula.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CursosFasesAulas extends Model
+{
+    use HasFactory;
+
+    protected $table = 'cursos_fases_aulas';
+
+    protected $fillable = [
+        'curso_fase_id',
+        'nome',
+        'descricao',
+        'link'
+    ];
+}
