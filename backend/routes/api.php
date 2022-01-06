48a17251e349f74e8c6e9af8a3ac28bfb669a457
Lorena <lorena@proesc.com>
2022-01-06 19:36:28 -0300

    Criada rota de cursos

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); */
+
+Route::prefix('v1')->group(function () {
+    Route::get('/cursos', 'CursosController@index');
+    Route::post('/cursos', 'CursosController@store');
+
+}
